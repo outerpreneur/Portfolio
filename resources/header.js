c325a8f20f2025d5c0a92fd53792f4f4d8945c8c
Aleksander <alek@outerpreneur.com>
2022-09-09 14:57:24 +0200

    box shadow added

--- resources/header.js
+++ resources/header.js
@@ -1,18 +1,34 @@
 // when I scroll down the page, at a certain point. make the header white
 
-const headerTag = document.querySelector("header") ;
+// defining constant
+
+const headerTag = document.querySelector("header");
+
+// functions
 
 const toggleHeader = function () {
-    const pixels = window.pageYOffset
-    if ( pixels > 60) {
-        headerTag.classList.add("scrolled")
-    } else {
-        headerTag.classList.remove("scrolled")
-    }
-}
+  const pixels = window.pageYOffset;
+  if (pixels > 60) {
+    headerTag.classList.add("scrolled");
+  } else {
+    headerTag.classList.remove("scrolled");
+  }
+};
 
-toggleHeader()
+const fadeBox = function () {
+  const pixels = window.pageYOffset;
+  const alpha = Math.min(pixels / 1000, 0.25);
+  headerTag.style.boxShadow = `0 0 10px rgba( 0, 0, 0, ${alpha})`;
+};
 
-document.addEventListener("scroll", function() {
-    toggleHeader()
-})
+// running the functions
+
+fadeBox();
+toggleHeader();
+
+// adding an event
+
+document.addEventListener("scroll", function () {
+  toggleHeader();
+  fadeBox();
+});
